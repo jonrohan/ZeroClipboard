c0b294014268020409160a1a44f38626a20f63f2
petehunt <floydophone@gmail.com>
2013-07-01 16:45:21 -0700

    Fix ZeroClipboard when used in CommonJS environment

--- src/javascript/end.js
+++ src/javascript/end.js
@@ -1,11 +1,10 @@
+window.ZeroClipboard = ZeroClipboard; // Needed to communicate with swf
 if (typeof module !== "undefined") {
   module.exports = ZeroClipboard;
 } else if (typeof define === "function" && define.amd) {
   define(function() {
     return ZeroClipboard;
   });
-} else {
-  window.ZeroClipboard = ZeroClipboard;
 }
 
 })();
